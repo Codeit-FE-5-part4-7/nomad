5483d4a71460a17063555ce9471d28f3c5d52276
eqypo9 <eqypo@naver.com>
2024-07-08 16:20:26 +0900

    fix: 깃 충돌

--- src/pages/preview/index.tsx
+++ src/pages/preview/index.tsx
@@ -1,29 +1,57 @@
 import React, { useState } from 'react';
-import CustomButton from '@/components/Button';
+import Button from '@/components/Button';
 import Card from '@/components/Card';
 import MyActibitiyCardInfo from '@/components/Card/myActibityCardInfo';
 import Pagination from '@/components/Pagination';
+import useModal from '@/hooks/useModal';
 
 function Index() {
+  const { openModal, closeModal } = useModal();
   const [currentPage, setCurrentPage] = useState(1);
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
   };
 
+  // Example Modal ------------------------
+  const handleOpenAlertModal = () => {
+    openModal({
+      modalType: 'alert',
+      content: 'Alert 모달입니다.',
+      btnName: ['확인'],
+    });
+  };
+
+  const handleOpenConfirmModal = () => {
+    openModal({
+      modalType: 'confirm',
+      content: 'Confirm 모달입니다.',
+      btnName: ['아니오', '취소하기'],
+      callBackFnc: () => {
+        alert('콜백 함수 실행');
+        closeModal();
+      },
+    });
+  };
+  // ------------------------------------
+
   return (
     <>
-      <CustomButton text='로그인 하기' color='black' />
+      <Button text='로그인 하기' color='black' />
       <hr />
-      <CustomButton text='로그인 하기' color='white' />
+      <Button text='로그인 하기' color='white' />
       <hr />
-      <CustomButton text='신청 불가' color='black' disabled />
+      <Button text='신청 불가' color='black' disabled />
       <hr />
       <Card image='/images/test123.png'>
         <MyActibitiyCardInfo title='테스트' price={10000} rating={4.31} reviewCount={2039} />
       </Card>
       <hr />
       <Pagination currentPage={currentPage} totalPages={12} onPageChange={handlePageChange} />
+      <hr />
+      <Button text='alert 모달 열기' color='white' onClick={handleOpenAlertModal} />
+      <hr />
+      <Button text='confirm 모달 열기' color='white' onClick={handleOpenConfirmModal} />
       <hr />
     </>
   );
