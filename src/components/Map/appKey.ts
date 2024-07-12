e05ceb3ba292e8b39166a386a08116592efbfea4
eqypo9 <eqypo@naver.com>
2024-07-13 01:50:28 +0900

    fix: 빌드 에러

--- src/components/Map/appKey.ts
+++ src/components/Map/appKey.ts
@@ -1,2 +1,1 @@
-const appKey = 'f7adb5c4574cc3a1412885d9f0aff326';
-export default appKey;
+// 빌드 에러 나서 일단 제거
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,7 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
           {/* async 하면 에러 생김 */}
-          <script type='text/javascript' src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=f7adb5c4574cc3a1412885d9f0aff326&libraries=services`} />
+          <script type='text/javascript' src='//dapi.kakao.com/v2/maps/sdk.js?appkey=f7adb5c4574cc3a1412885d9f0aff326&libraries=services' />
         </body>
       </Html>
     );
